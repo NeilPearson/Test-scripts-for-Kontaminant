0eac78387abe66479e9de201699eea4079b9f3c0
Neil Pearson (TGAC) <Neil.Pearson@tgac.ac.uk>
2015-12-10 14:59:07 +0000

    Change to the way fake reads are made from reference sequences
    …in order to make it work properly.

--- contaminate_reads_pe.pl
+++ contaminate_reads_pe.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 use List::Util qw(shuffle);
+use File::Basename;
 
 # Takes a reads file, a contaminant reads file and an integer as input.
 # Mixes the supplied number of reads from the contaminant in with the other reads, shuffles them up so they're in a different order, and
@@ -11,17 +12,21 @@ my $r1_file = $ARGV[0];
 my $r2_file = $ARGV[1];
 my $contaminant_file = $ARGV[2];
 my $use_n_contaminant_reads = $ARGV[3];
+my $contaminant = basename($contaminant_file);
 
 open(READS, "<", $r1_file) or die "Cannot open reads file $r1_file: $!\n";
 my @r1lines = <READS>;   close READS;
 
 my @r1s = ();
 my @buffer = ();
+my $maxread = 0;
 foreach my $line (@r1lines) {
     push @buffer, $line;
     if (@buffer >= 4) {
         my $read = join '', @buffer;
+        chomp $read;
         push @r1s, $read;
+        if (length $buffer[1] > $maxread) { $maxread = length $buffer[1]; }
         @buffer = ();
     }
 }
@@ -36,7 +41,9 @@ foreach my $line (@r2lines) {
     push @buffer, $line;
     if (@buffer >= 4) {
         my $read = join '', @buffer;
+        chomp $read;
         push @r2s, $read;
+        if (length $buffer[1] > $maxread) { $maxread = length $buffer[1]; }
         @buffer = ();
     }
 }
@@ -44,38 +51,78 @@ foreach my $line (@r2lines) {
 
 open(CONTAMINANTS, "<", $contaminant_file) or die "Cannot open contaminant file $contaminant_file: $!\n";
 my @contaminantlines = <CONTAMINANTS>;
+# Assume contaminant is in FASTA format
+# (On the grounds that this program is meant to generate fake reads from a reference genome).
+# However, contaminant reads need to be output in FASTQ format, which means adding some extra stuff.
+# (Assume quality scores don't matter, so they can be anything).
+# Oh right, and I'll need to match the length of the contaminant reads to those in the existing dataset.
+# That will mean getting the max length of line in that data (see above).
+# (Assume Illumina-type data with a fixed max read length).
+# Oh, and one more thing: the contaminant ref data will be quite long, mostly, and in one or a few contigs.
+# I'll have to handle that in an appropriate way. 
 
-my @contaminant_reads = ();
+my @contaminant_contigs = ();
 @buffer = ();
 foreach my $line (@contaminantlines) {
-    push @buffer, $line;
-    if (@buffer >= 4) {
-        my $read = join '', @buffer;
-        push @contaminant_reads, $read;
+    if (($line =~ /^>/) && (@buffer)) {
+        # FASTA read may be spread over multiple lines. Header will always be in the first line however.
+        my $header = shift @buffer;
+        my $seq = join '', @buffer;
+        my $contig = "$header\n$seq";
+        # A sanity check: only store this contig if the seq is longer than the length of fake read we want.
+        if (length $seq > $maxread) { push @contaminant_contigs, $contig; }
         @buffer = ();
     }
+    chomp $line;
+    push @buffer, $line;
+}
+# Make sure to get the last contig out of the buffer. This is important because there may only be one sequence in the file.
+if (@buffer) {
+    # FASTA read may be spread over multiple lines. Header will always be in the first line however.
+    my $header = shift @buffer;
+    my $seq = join '', @buffer;
+    my $contig = "$header\n$seq";
+    # A sanity check: only store this contig if the seq is longer than the length of fake read we want.
+    if (length $seq > $maxread) { push @contaminant_contigs, $contig; }
+    @buffer = ();
 }
 @contaminantlines = ();
 
-# OK, pick some contaminant reads and put them in with the rest.
+# Now, it's possible to generate a number of fake FASTQ reads and put them in with the rest.
+my @qscores = qw/A B C D E F/;
 for my $i (1..$use_n_contaminant_reads) {
-    my $read = $contaminant_reads[$i-1];
+    # Select a contig at random. (Yeah, this will give a bias towards the shorter contigs, if there's more than one. I'll do a weighted random choice in a later version).
+    my $contig = $contaminant_contigs[rand @contaminant_contigs];
     # Tag the read as a contaminant so I can identify it later.
-    my @sp1 = split /\n/, $read;
+    # This tag also needs to be unique, so I'll probably need to add the number stored in $i or something.
+    my @sp1 = split /\n/, $contig;
     my $tag = $sp1[0];
     chomp $tag;
-    $tag .= "_CONTAMINANT\n";
-    $read = join '', @sp1;
+    $tag .= " CONTAMINANT $contaminant uid $i";
+    $tag =~ s/\>/\@/;
+    $sp1[0] = $tag;
+    # Get a subset of the sequence with an appropriate length
+    my $seq = substr($sp1[1], rand(length($sp1[1]) - $maxread - 1), $maxread);
+    $sp1[1] = $seq;
+    $sp1[2] = "+";
+    my $qstr = ();
+    for (1..length($sp1[1])) { $qstr .= $qscores[rand @qscores]; }
+    $sp1[3] = $qstr;
     
-    my $r1 = $read;
-    my $r2 = $read;
+    my $fakeread = join "\n", @sp1;
     
-    @sp1 = split /\n/, $r2;
-    my $seq = $sp1[1];
+    my $r1 = $fakeread;
+    my $r2 = $fakeread;
+    
+    # Read 2 needs to be the reverse complement of read 1
+    # (Yes, I know that due to the way paired ends work, I should pick a sequence a bit further along the contig too.
+    # I'll do that in the future).
+    my @sp2 = split /\n/, $r2;
+    $seq = $sp2[1];
     chomp $seq;
     $seq = reverse_complement($seq);
-    $sp1[1] = "$seq\n";
-    $r2 = join '', @sp1;
+    $sp2[1] = $seq;
+    $r2 = join "\n", @sp2;
     
     push @r1s, $r1;
     push @r2s, $r2;
@@ -86,17 +133,22 @@ my @indexes = (1..@r1s);
 
 # Oh man, I can't just dump output to stdout any more. What a pain.
 # I'll have to modify the input filenames and set up those as output.
+
+$contaminant =~ s/.fasta//g;
 my $r1out = $r1_file;
 my $r2out = $r2_file;
-$r1out =~ s/.fastq/_CONTAMINATED.fastq/;
-$r2out =~ s/.fastq/_CONTAMINATED.fastq/;
+$r1out =~ s/.fastq/_CONTAMINATED_$contaminant.fastq/;
+$r2out =~ s/.fastq/_CONTAMINATED_$contaminant.fastq/;
 open(R1OUT, ">", $r1out) or die "Cannot open output $r1out: $!\n";
 open(R2OUT, ">", $r2out) or die "Cannot open output $r2out: $!\n";
+my $c = 0;
 foreach my $i (@indexes) {
+    $c ++;
     my $r1 = $r1s[$i-1];
     my $r2 = $r2s[$i-1];
     print R1OUT $r1;
     print R2OUT $r2;
+    print R1OUT "\n"; print R2OUT "\n";
 }
 close R1OUT;
 close R2OUT;
--- contaminate_reads_se.pl
+++ contaminate_reads_se.pl
@@ -4,22 +4,25 @@ use List::Util qw(shuffle);
 
 # Takes a reads file, a contaminant reads file and an integer as input.
 # Mixes the supplied number of reads from the contaminant in with the other reads, shuffles them up so they're in a different order, and
-# dumps everything out to stdout (from which it can be redirected to a file).
+# prints the resultant dataset to a new file.
 
 my $reads_file = $ARGV[0];
 my $contaminant_file = $ARGV[1];
 my $use_n_contaminant_reads = $ARGV[2];
+my $contaminant = basename($contaminant_file);
 
 open(READS, "<", $reads_file) or die "Cannot open reads file $reads_file: $!\n";
 my @readslines = <READS>;   close READS;
 
 my @reads = ();
 my @buffer = ();
+my $maxread = 0;
 foreach my $line (@readslines) {
     push @buffer, $line;
     if (@buffer >= 4) {
         my $read = join '', @buffer;
         push @reads, $read;
+        if (length $buffer[1] > $maxread) { $maxread = length $buffer[1]; }
         @buffer = ();
     }
 }
@@ -27,34 +30,76 @@ foreach my $line (@readslines) {
 
 open(CONTAMINANTS, "<", $contaminant_file) or die "Cannot open contaminant file $contaminant_file: $!\n";
 my @contaminantlines = <CONTAMINANTS>;
+# Assume contaminant is in FASTA format
+# (On the grounds that this program is meant to generate fake reads from a reference genome).
+# However, contaminant reads need to be output in FASTQ format, which means adding some extra stuff.
+# (Assume quality scores don't matter, so they can be anything).
+# Oh right, and I'll need to match the length of the contaminant reads to those in the existing dataset.
+# That will mean getting the max length of line in that data (see above).
+# (Assume Illumina-type data with a fixed max read length).
+# Oh, and one more thing: the contaminant ref data will be quite long, mostly, and in one or a few contigs.
+# I'll have to handle that in an appropriate way. 
 
-my @contaminant_reads = ();
+my @contaminant_contigs = ();
 @buffer = ();
 foreach my $line (@contaminantlines) {
-    push @buffer, $line;
-    if (@buffer >= 4) {
-        my $read = join '', @buffer;
-        push @contaminant_reads, $read;
+    if (($line =~ /^>/) && (@buffer)) {
+        # FASTA read may be spread over multiple lines. Header will always be in the first line however.
+        my $header = shift @buffer;
+        my $seq = join '', @buffer;
+        my $contig = "$header\n$seq";
+        # A sanity check: only store this contig if the seq is longer than the length of fake read we want.
+        if (length $seq > $maxread) { push @contaminant_contigs, $contig; }
         @buffer = ();
     }
+    chomp $line;
+    push @buffer, $line;
+}
+# Make sure to get the last contig out of the buffer. This is important because there may only be one sequence in the file.
+if (@buffer) {
+    # FASTA read may be spread over multiple lines. Header will always be in the first line however.
+    my $header = shift @buffer;
+    my $seq = join '', @buffer;
+    my $contig = "$header\n$seq";
+    # A sanity check: only store this contig if the seq is longer than the length of fake read we want.
+    if (length $seq > $maxread) { push @contaminant_contigs, $contig; }
+    @buffer = ();
 }
 @contaminantlines = ();
 
-# OK, pick some contaminant reads and put them in with the rest.
+# Now, it's possible to generate a number of fake FASTQ reads and put them in with the rest.
+my @qscores = qw/A B C D E F/;
 for my $i (1..$use_n_contaminant_reads) {
+    # Select a contig at random. (Yeah, this will give a bias towards the shorter contigs, if there's more than one. I'll do a weighted random choice in a later version).
+    my $contig = $contaminant_contigs[rand @contaminant_contigs];
     # Tag the read as a contaminant so I can identify it later.
-    my $read = $contaminant_reads[$i-1];
-    my @sp1 = split /\n/, $read;
+    # This tag also needs to be unique, so I'll probably need to add the number stored in $i or something.
+    my @sp1 = split /\n/, $contig;
     my $tag = $sp1[0];
     chomp $tag;
-    $tag .= "_CONTAMINANT\n";
-    $read = join '', @sp1;
+    $tag .= " CONTAMINANT $contaminant uid $i";
+    $tag =~ s/\>/\@/;
+    $sp1[0] = $tag;
+    # Get a subset of the sequence with an appropriate length
+    my $seq = substr($sp1[1], rand(length($sp1[1]) - $maxread - 1), $maxread);
+    $sp1[1] = $seq;
+    $sp1[2] = "+";
+    my $qstr = ();
+    for (1..length($sp1[1])) { $qstr .= $qscores[rand @qscores]; }
+    $sp1[3] = $qstr;
     
-    push @reads, $read;
+    my $fakeread = join "\n", @sp1;
+    push @reads, $fakeread;
 }
 
 @reads = shuffle(@reads);
 
+# For the sake of consistency with the PE version, I may as well have this output a file.
+$contaminant =~ s/.fasta//g;
+my $out = $reads_file;
+$out =~ s/.fastq/_CONTAMINATED_$contaminant.fastq/;
+open(OUT, ">", $out) or die "Cannot open output $out: $!\n";
 foreach my $read (@reads) {
-    print $read;
+    print OUT $read;
 }
+close OUT;
